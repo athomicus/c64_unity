8d1b80a67e11809d114a0c7a4a4fe7190828e35d
athomicus <96152125+athomicus@users.noreply.github.com>
2026-04-26 22:33:08 +0200

    Delete mapka_half.h

--- mapka_half.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAPKA_HALF_H
-#define MAPKA_HALF_H
-
-// Pol mapy C64: 160x200px, 20x25 komorek 8x8
-// bitmap: 4000 bajtow [0..3999], coloram: 500 bajtow [4000..4499]
-//const unsigned char mapa[9000] = {};
-
-#endif
